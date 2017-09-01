e2a984f731638dd5ce21e5438dac2589b7e102d9
cjvil <christiejvillanueva@gmail.com>
2017-09-01 15:38:41 -0700

    passes sending and fetching tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,49 @@
-// YOUR CODE HERE:
+var app = {
+
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+
+  init: function() {
+  
+  },
+  
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      //data: JSON.stringify(message),
+      data: (message), // changed from stringify to pass tests
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });  
+  },
+
+  fetch: function() {
+    $.ajax({
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      contentType: 'PlainObject',
+      success: function (data) {
+        console.log('chatterbox: data recieved');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message', data);
+      }
+      
+    });
+  
+
+  }
+
+ 
+
+
+};
+
